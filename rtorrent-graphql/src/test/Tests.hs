8405f2530c30e6afda488f03ba0c440864d6f045
Guillaume Desforges <aceus02@gmail.com>
2020-12-12 19:36:30 +0100

    Working graphql test

--- rtorrent-graphql/src/test/Tests.hs
+++ rtorrent-graphql/src/test/Tests.hs
@@ -1,18 +1,46 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+
 module Tests where
 
-import Test.HTTP
-import Test.Tasty
-import Test.Tasty.HUnit
+import Network.HTTP.Types (methodPost)
+import Network.HTTP.Types.Header (hContentType)
+import Network.Wai (Application)
+import Server (getDefaultServer)
+import System.IO.Unsafe (unsafePerformIO)
+import System.Process (ProcessHandle, spawnProcess, terminateProcess)
+import Test.Hspec (Spec, describe, it)
+import Test.Hspec.Wai (request, shouldRespondWith, with)
+import Test.Hspec.Wai.JSON (json)
+import Test.Tasty (TestTree, defaultMain, testGroup, withResource)
+import Test.Tasty.Hspec (testSpec)
 
-main = defaultMain tests
+main :: IO ()
+main = tests >>= defaultMain
 
-tests :: TestTree
-tests = testGroup "Tests" [integrationTests]
+tests :: IO TestTree
+tests =
+  let integrationTests :: IO TestTree
+      integrationTests =
+        let openRtorrent :: IO ProcessHandle
+            openRtorrent = do
+              -- spawn rtorrent
+              spawnProcess
+                "rtorrent"
+                [ "-n", -- do not load ~/.rtorrent.rc
+                  "-o import=./rtorrent.rc" -- load ./rtorrent.rc instead
+                ]
 
-integrationTests :: TestTree
-integrationTests = testGroup "Integration" [apiResponds]
+            closeRtorrent :: ProcessHandle -> IO ()
+            closeRtorrent handle = terminateProcess handle
 
-apiResponds = httpTestCase "API responds" "http://localhost:3000" $
-  do
-    post "/api" "{\"query\": \"{ hello }\"}"
-    return ()
+            apiResponds :: IO Application -> Spec
+            apiResponds app = with app $ do
+              describe "Query .hello" $ do
+                it "returns data" $ do
+                  let testRequest = request methodPost "/api" [(hContentType, "application/json")] [json|{query: "{hello}"}|]
+                  testRequest `shouldRespondWith` [json|{data: {hello: "Hello world"}}|]
+         in return $
+              withResource openRtorrent closeRtorrent $ \_ ->
+                withResource getDefaultServer mempty $ \app -> unsafePerformIO $ testSpec "Integration" (apiResponds app)
+   in testGroup "Tests" . (: []) <$> integrationTests
